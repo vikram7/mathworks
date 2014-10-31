cd87f8a97123f03ca203e0660576053f9383a788
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-10-30 22:26:25 -0400

    added feature tests for user creating course (successfully and unsuccesfully)

--- spec/factories/course.rb
+++ spec/factories/course.rb
@@ -1,12 +1,8 @@
 FactoryGirl.define do
-  factory :comment do
-    sequence :user_id do |n|
-      "#{n}"
-    end
-
-    text "This is my first comment"
-
-    association :review
-    association :user
+  factory :course do
+    title "Getting Started with MATLAB"
+    description "Welcome to this MATLAB Video tutorial. If you have never used MATLAB before, this demonstration will get you started and show you where to go to next to learn more."
+    url "http://www.mathworks.com/examples/matlab/3760-getting-started-with-matlab"
+    association :source
   end
 end
