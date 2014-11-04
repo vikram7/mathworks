680e5ccc3220741667ec4e6ccf2fe45bf47d7d04
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-11-03 22:30:26 -0500

    removed comment from models test

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -5,11 +5,8 @@ describe Course do
 
   describe 'validations' do
     it { should validate_presence_of(:title) }
-
     it { should validate_presence_of(:url ) }
-    # it { should have_valid(:url).when('http://www.google.com', 'https://www.google.com', 'www.google.com', 'google.com') }
     it { should validate_presence_of(:source) }
-
   end
 
   describe 'associations' do
